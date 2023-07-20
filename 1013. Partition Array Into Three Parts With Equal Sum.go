c7f938c2b5e11dea0db2a12642d3b26f5f146fd0
oleg.vantsevich <oleg.vantsevich@effective-soft.com>
2023-07-20 16:42:38 +0300

    1013. Partition Array Into Three Parts With Equal Sum

---
+++ 1013. Partition Array Into Three Parts With Equal Sum.go
@@ -0,0 +1,47 @@
+package main
+
+func sum(arr []int) int {
+	var s int
+
+	for _, n := range arr {
+		s += n
+	}
+
+	return s
+}
+
+func canThreePartsEqualSum(arr []int) bool {
+	s := sum(arr)
+	if s%3 != 0 {
+		return false
+	}
+
+	var p1 int
+	var i int
+	for {
+		p1 += arr[i]
+		i++
+		if !(p1 != s/3 && i < len(arr)) {
+			break
+		}
+	}
+	if p1 != s/3 || i == len(arr) {
+		return false
+	}
+
+	var p2 int
+	j := i
+	for {
+		p2 += arr[j]
+		j++
+		if p2 != s/3 && j < len(arr) {
+			continue
+		}
+		break
+	}
+	if p2 != s/3 || j == len(arr) {
+		return false
+	}
+
+	return true
+}
